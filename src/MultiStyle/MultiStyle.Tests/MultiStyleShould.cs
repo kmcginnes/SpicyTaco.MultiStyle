500f0858c1609ebc9dec43e830663824defa59b7
Kris McGinnes <kmcginnes@me.com>
2014-10-03 19:46:32 -0500

    Added first test for parsing

---
+++ src/MultiStyle/MultiStyle.Tests/MultiStyleShould.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using Xunit.Extensions;
+
+namespace MultiStyle.Tests
+{
+    public class MultiStyleShould
+    {
+        [Fact]
+        public void Foo()
+        {
+            true.Should().BeTrue();
+        }
+
+        [Theory]
+        [InlineData("foo", new[] { "foo" })]
+        [InlineData("foo bar", new[] { "foo", "bar" })]
+        [InlineData("baz fiz", new[] { "baz", "fiz" })]
+        [InlineData("foo bar baz fiz", new[] { "foo", "bar", "baz", "fiz" })]
+        [InlineData("foo ", new[] { "foo" })]
+        [InlineData(" foo", new[] { "foo" })]
+        [InlineData("", new string[] { })]
+        [InlineData("   ", new string[] { })]
+        [InlineData(null, new string[] { })]
+        public void ParseStyleNamesFromString(string input, IEnumerable<string> output)
+        {
+            var result = MultiStyleExtension.Parse(input);
+
+            result.Should().ContainInOrder(output);
+        }
+    }
+
+    public class MultiStyleExtension
+    {
+        public static IEnumerable<string> Parse(string styleNames)
+        {
+            return (styleNames ?? string.Empty).Split(' ');
+        }
+    }
+}
